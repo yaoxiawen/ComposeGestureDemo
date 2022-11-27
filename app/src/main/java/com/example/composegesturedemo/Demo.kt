3f05280bd1ef14d9ddfd284b21e64e9320243d9c
yaoxiawen <735649296@qq.com>
2022-11-27 17:18:15 +0800

    滚动修饰符

--- app/src/main/java/com/example/composegesturedemo/Demo.kt
+++ app/src/main/java/com/example/composegesturedemo/Demo.kt
@@ -5,6 +5,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -13,6 +15,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
 
 /**
  * 点击手势
@@ -55,5 +58,48 @@ fun Demo1() {
                 .background(Color.LightGray)
                 .padding(horizontal = 50.dp, vertical = 40.dp)
         )
+    }
+}
+
+/**
+ * 滚动修饰符
+ * verticalScroll & horizontalScroll,在元素内容边界大于最大尺寸约束时滚动元素
+ * 借助ScrollState可以更改滚动位置或获取当前状态。如需使用默认参数创建此列表，请使用rememberScrollState
+ */
+@Composable
+fun Demo2() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center,
+        modifier = Modifier
+    ) {
+        Column(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .size(100.dp)
+                .verticalScroll(rememberScrollState())
+        ) {
+            repeat(10) {
+                Text(text = "Item $it", modifier = Modifier.padding(2.dp))
+            }
+        }
+
+        Spacer(modifier = Modifier.width(20.dp))
+
+        val state = rememberScrollState()
+        LaunchedEffect(Unit) {
+            delay(1000)
+            state.animateScrollTo(500)
+        }
+        Column(
+            modifier = Modifier
+                .background(Color.LightGray)
+                .size(100.dp)
+                .verticalScroll(state)
+        ) {
+            repeat(10) {
+                Text(text = "Item $it", modifier = Modifier.padding(2.dp))
+            }
+        }
     }
 }
