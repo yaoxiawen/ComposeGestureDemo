688037a6207738c0a3e271e61ab6e0655ec38803
yaoxiawen <735649296@qq.com>
2022-11-27 17:04:53 +0800

    点击手势,clickable修饰符,pointerInput修饰符

--- app/src/main/java/com/example/composegesturedemo/Demo.kt
+++ app/src/main/java/com/example/composegesturedemo/Demo.kt
@@ -1,9 +1,18 @@
 package com.example.composegesturedemo
 
+import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.*
-import androidx.compose.runtime.Composable
+import androidx.compose.material.Text
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 /**
  * 点击手势
@@ -15,8 +24,36 @@ fun Demo1() {
     Row(
         verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Center,
-        modifier = Modifier.fillMaxSize()
+        modifier = Modifier
     ) {
-
+        var count by remember {
+            mutableStateOf(0)
+        }
+        Text(text = "$count",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .clickable {
+                    ++count
+                }
+                .wrapContentSize()
+                .background(Color.LightGray)
+                .padding(horizontal = 50.dp, vertical = 40.dp)
+        )
+        Spacer(modifier = Modifier.width(20.dp))
+        Text(text = "$count",
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .pointerInput(Unit) {
+                    detectTapGestures(
+                        onPress = { Log.d("pointerInput", "called when the gesture starts") },
+                        onDoubleTap = { Log.d("pointerInput", "called on double tap") },
+                        onLongPress = { Log.d("pointerInput", "called on long tap") },
+                        onTap = { Log.d("pointerInput", "called on tap") }
+                    )
+                }
+                .wrapContentSize()
+                .background(Color.LightGray)
+                .padding(horizontal = 50.dp, vertical = 40.dp)
+        )
     }
 }
